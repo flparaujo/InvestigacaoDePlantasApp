c5699de488a9082719ed5a82f0971c66d1f53192
Felipe Andrade <araujoandrade.flp@gmail.com>
2019-09-23 16:50:00 -0300

    Change directory name

--- app/src/main/java/jordean/profbio/ufpb/mestrado/investigacaodeplantas/util/AppStrings.java
+++ app/src/main/java/jordean/profbio/ufpb/mestrado/investigacaodeplantas/util/AppStrings.java
@@ -4,7 +4,7 @@ public class AppStrings {
 
     public static final String FOTO_NAO_TIRADA = "Foto não tirada";
     public static final String ERRO_TIRAR_FOTO = "Erro ao tirar foto";
-    public static final String NOME_DIRETORIO_APP = "EnsinoInvestigativo";
+    public static final String NOME_DIRETORIO_APP = "InvestigacaoDePlantas";
     public static final String ARQUIVO_NAO_ENCONTRADO = "Arquivo não encontrado! ";
     public static final String ABRIR_COM = "Abrir com ...";
 }
